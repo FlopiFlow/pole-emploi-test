c13889a8f720a795a1a5c598a00c9a9f851bb844
flowpi <chapeu.florent@gmail.com>
2022-07-24 16:44:39 +0200

    client App.js to class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
+import React, { Component } from "react"
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+class App extends React.Component {
 
-  return (
-    <div className="App">
-      <header>
-        <div class="brand inline">
-          Emploi France
-        </div>
-        <div class="brand-description inline">
-          <small>Données actualisées <b>en direct</b> à partir des 150 dernières offres publiées par <a href="https://www.pole-emploi.fr/accueil/" target="_blank"><b>Pôle emploi</b></a></small>
-        </div>
-      </header>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <div class="brand inline">
+            Emploi France
+          </div>
+          <div class="brand-description inline">
+            <small>Données actualisées <b>en direct</b> à partir des 150 dernières offres publiées par <a href="https://www.pole-emploi.fr/accueil/" target="_blank"><b>Pôle emploi</b></a></small>
+          </div>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
